91e39f44b3a930c5dd33b45297f037e24db61098
GordienkoRU <r.u.gordienko@gmail.com>
2017-12-05 16:31:53 +0300

    Update bundle

--- bin/bundle.js
+++ bin/bundle.js
@@ -952,7 +952,7 @@ var _PlayBoard = __webpack_require__(27);
 
 var _PlayBoard2 = _interopRequireDefault(_PlayBoard);
 
-var _style = __webpack_require__(46);
+var _style = __webpack_require__(44);
 
 var _style2 = _interopRequireDefault(_style);
 
@@ -18289,27 +18289,27 @@ var _UpperControlBlock = __webpack_require__(28);
 
 var _UpperControlBlock2 = _interopRequireDefault(_UpperControlBlock);
 
-var _PlayerStatusPanels = __webpack_require__(34);
+var _PlayerStatusPanels = __webpack_require__(32);
 
 var _PlayerStatusPanels2 = _interopRequireDefault(_PlayerStatusPanels);
 
-var _BottomControlBlock = __webpack_require__(37);
+var _BottomControlBlock = __webpack_require__(35);
 
 var _BottomControlBlock2 = _interopRequireDefault(_BottomControlBlock);
 
-var _Dice = __webpack_require__(40);
+var _Dice = __webpack_require__(38);
 
 var _Dice2 = _interopRequireDefault(_Dice);
 
-var _config = __webpack_require__(43);
+var _config = __webpack_require__(41);
 
 var _config2 = _interopRequireDefault(_config);
 
-var _services = __webpack_require__(44);
+var _services = __webpack_require__(42);
 
 var _services2 = _interopRequireDefault(_services);
 
-var _initialState = __webpack_require__(45);
+var _initialState = __webpack_require__(43);
 
 var _initialState2 = _interopRequireDefault(_initialState);
 
@@ -18492,7 +18492,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var UpperControlBlock = function UpperControlBlock(props) {
   return _react2.default.createElement(
     'div',
-    { 'class': 'upper-container-wrapper' },
+    { className: 'upper-container-wrapper' },
     _react2.default.createElement(
       'div',
       { className: 'upper-control-container' },
@@ -18580,9 +18580,7 @@ exports.default = ReferenceBtn;
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-
-var _require = __webpack_require__(32),
-    ipcRenderer = _require.ipcRenderer;
+//import { ipcRenderer } from 'electron';
 
 var handleReference = function handleReference() {
   //  ipcRenderer.send('asynchronous-message', 'SHOW_REFERENCE');
@@ -18592,254 +18590,6 @@ exports.default = handleReference;
 
 /***/ }),
 /* 32 */
-/***/ (function(module, exports, __webpack_require__) {
-
-/* WEBPACK VAR INJECTION */(function(__dirname) {var fs = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"fs\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()))
-var path = __webpack_require__(33)
-
-var pathFile = path.join(__dirname, 'path.txt')
-
-if (fs.existsSync(pathFile)) {
-  module.exports = path.join(__dirname, fs.readFileSync(pathFile, 'utf-8'))
-} else {
-  throw new Error('Electron failed to install correctly, please delete node_modules/electron and try installing again')
-}
-
-/* WEBPACK VAR INJECTION */}.call(exports, "/"))
-
-/***/ }),
-/* 33 */
-/***/ (function(module, exports, __webpack_require__) {
-
-/* WEBPACK VAR INJECTION */(function(process) {// Copyright Joyent, Inc. and other Node contributors.
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to permit
-// persons to whom the Software is furnished to do so, subject to the
-// following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
-// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
-// USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-// resolves . and .. elements in a path array with directory names there
-// must be no slashes, empty elements, or device names (c:\) in the array
-// (so also no leading and trailing slashes - it does not distinguish
-// relative and absolute paths)
-function normalizeArray(parts, allowAboveRoot) {
-  // if the path tries to go above the root, `up` ends up > 0
-  var up = 0;
-  for (var i = parts.length - 1; i >= 0; i--) {
-    var last = parts[i];
-    if (last === '.') {
-      parts.splice(i, 1);
-    } else if (last === '..') {
-      parts.splice(i, 1);
-      up++;
-    } else if (up) {
-      parts.splice(i, 1);
-      up--;
-    }
-  }
-
-  // if the path is allowed to go above the root, restore leading ..s
-  if (allowAboveRoot) {
-    for (; up--; up) {
-      parts.unshift('..');
-    }
-  }
-
-  return parts;
-}
-
-// Split a filename into [root, dir, basename, ext], unix version
-// 'root' is just a slash, or nothing.
-var splitPathRe =
-    /^(\/?|)([\s\S]*?)((?:\.{1,2}|[^\/]+?|)(\.[^.\/]*|))(?:[\/]*)$/;
-var splitPath = function(filename) {
-  return splitPathRe.exec(filename).slice(1);
-};
-
-// path.resolve([from ...], to)
-// posix version
-exports.resolve = function() {
-  var resolvedPath = '',
-      resolvedAbsolute = false;
-
-  for (var i = arguments.length - 1; i >= -1 && !resolvedAbsolute; i--) {
-    var path = (i >= 0) ? arguments[i] : process.cwd();
-
-    // Skip empty and invalid entries
-    if (typeof path !== 'string') {
-      throw new TypeError('Arguments to path.resolve must be strings');
-    } else if (!path) {
-      continue;
-    }
-
-    resolvedPath = path + '/' + resolvedPath;
-    resolvedAbsolute = path.charAt(0) === '/';
-  }
-
-  // At this point the path should be resolved to a full absolute path, but
-  // handle relative paths to be safe (might happen when process.cwd() fails)
-
-  // Normalize the path
-  resolvedPath = normalizeArray(filter(resolvedPath.split('/'), function(p) {
-    return !!p;
-  }), !resolvedAbsolute).join('/');
-
-  return ((resolvedAbsolute ? '/' : '') + resolvedPath) || '.';
-};
-
-// path.normalize(path)
-// posix version
-exports.normalize = function(path) {
-  var isAbsolute = exports.isAbsolute(path),
-      trailingSlash = substr(path, -1) === '/';
-
-  // Normalize the path
-  path = normalizeArray(filter(path.split('/'), function(p) {
-    return !!p;
-  }), !isAbsolute).join('/');
-
-  if (!path && !isAbsolute) {
-    path = '.';
-  }
-  if (path && trailingSlash) {
-    path += '/';
-  }
-
-  return (isAbsolute ? '/' : '') + path;
-};
-
-// posix version
-exports.isAbsolute = function(path) {
-  return path.charAt(0) === '/';
-};
-
-// posix version
-exports.join = function() {
-  var paths = Array.prototype.slice.call(arguments, 0);
-  return exports.normalize(filter(paths, function(p, index) {
-    if (typeof p !== 'string') {
-      throw new TypeError('Arguments to path.join must be strings');
-    }
-    return p;
-  }).join('/'));
-};
-
-
-// path.relative(from, to)
-// posix version
-exports.relative = function(from, to) {
-  from = exports.resolve(from).substr(1);
-  to = exports.resolve(to).substr(1);
-
-  function trim(arr) {
-    var start = 0;
-    for (; start < arr.length; start++) {
-      if (arr[start] !== '') break;
-    }
-
-    var end = arr.length - 1;
-    for (; end >= 0; end--) {
-      if (arr[end] !== '') break;
-    }
-
-    if (start > end) return [];
-    return arr.slice(start, end - start + 1);
-  }
-
-  var fromParts = trim(from.split('/'));
-  var toParts = trim(to.split('/'));
-
-  var length = Math.min(fromParts.length, toParts.length);
-  var samePartsLength = length;
-  for (var i = 0; i < length; i++) {
-    if (fromParts[i] !== toParts[i]) {
-      samePartsLength = i;
-      break;
-    }
-  }
-
-  var outputParts = [];
-  for (var i = samePartsLength; i < fromParts.length; i++) {
-    outputParts.push('..');
-  }
-
-  outputParts = outputParts.concat(toParts.slice(samePartsLength));
-
-  return outputParts.join('/');
-};
-
-exports.sep = '/';
-exports.delimiter = ':';
-
-exports.dirname = function(path) {
-  var result = splitPath(path),
-      root = result[0],
-      dir = result[1];
-
-  if (!root && !dir) {
-    // No dirname whatsoever
-    return '.';
-  }
-
-  if (dir) {
-    // It has a dirname, strip trailing slash
-    dir = dir.substr(0, dir.length - 1);
-  }
-
-  return root + dir;
-};
-
-
-exports.basename = function(path, ext) {
-  var f = splitPath(path)[2];
-  // TODO: make this comparison case-insensitive on windows?
-  if (ext && f.substr(-1 * ext.length) === ext) {
-    f = f.substr(0, f.length - ext.length);
-  }
-  return f;
-};
-
-
-exports.extname = function(path) {
-  return splitPath(path)[3];
-};
-
-function filter (xs, f) {
-    if (xs.filter) return xs.filter(f);
-    var res = [];
-    for (var i = 0; i < xs.length; i++) {
-        if (f(xs[i], i, xs)) res.push(xs[i]);
-    }
-    return res;
-}
-
-// String.prototype.substr - negative index don't work in IE8
-var substr = 'ab'.substr(-1) === 'b'
-    ? function (str, start, len) { return str.substr(start, len) }
-    : function (str, start, len) {
-        if (start < 0) start = str.length + start;
-        return str.substr(start, len);
-    }
-;
-
-/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
-
-/***/ }),
-/* 34 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -18853,7 +18603,7 @@ var _react = __webpack_require__(0);
 
 var _react2 = _interopRequireDefault(_react);
 
-var _createPlayers = __webpack_require__(35);
+var _createPlayers = __webpack_require__(33);
 
 var _createPlayers2 = _interopRequireDefault(_createPlayers);
 
@@ -18870,7 +18620,7 @@ var PlayerStatusPanels = function PlayerStatusPanels(props) {
 exports.default = PlayerStatusPanels;
 
 /***/ }),
-/* 35 */
+/* 33 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -18886,7 +18636,7 @@ var _react = __webpack_require__(0);
 
 var _react2 = _interopRequireDefault(_react);
 
-var _PlayerArea = __webpack_require__(36);
+var _PlayerArea = __webpack_require__(34);
 
 var _PlayerArea2 = _interopRequireDefault(_PlayerArea);
 
@@ -18905,7 +18655,7 @@ var createPlayers = function createPlayers(playersArr, activePlayerIndex) {
 exports.default = createPlayers;
 
 /***/ }),
-/* 36 */
+/* 34 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -18945,7 +18695,7 @@ var PlayerBlock = function PlayerBlock(props) {
 exports.default = PlayerBlock;
 
 /***/ }),
-/* 37 */
+/* 35 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -18959,11 +18709,11 @@ var _react = __webpack_require__(0);
 
 var _react2 = _interopRequireDefault(_react);
 
-var _HoldBtn = __webpack_require__(38);
+var _HoldBtn = __webpack_require__(36);
 
 var _HoldBtn2 = _interopRequireDefault(_HoldBtn);
 
-var _RollBtn = __webpack_require__(39);
+var _RollBtn = __webpack_require__(37);
 
 var _RollBtn2 = _interopRequireDefault(_RollBtn);
 
@@ -18993,7 +18743,7 @@ var BottomControlBlock = function BottomControlBlock(props) {
 exports.default = BottomControlBlock;
 
 /***/ }),
-/* 38 */
+/* 36 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -19024,7 +18774,7 @@ var HoldBtn = function HoldBtn(props) {
 exports.default = HoldBtn;
 
 /***/ }),
-/* 39 */
+/* 37 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -19055,7 +18805,7 @@ var RollBtn = function RollBtn(props) {
 exports.default = RollBtn;
 
 /***/ }),
-/* 40 */
+/* 38 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -19071,11 +18821,11 @@ var _react = __webpack_require__(0);
 
 var _react2 = _interopRequireDefault(_react);
 
-var _pointsHandler = __webpack_require__(41);
+var _pointsHandler = __webpack_require__(39);
 
 var _pointsHandler2 = _interopRequireDefault(_pointsHandler);
 
-var _Point = __webpack_require__(42);
+var _Point = __webpack_require__(40);
 
 var _Point2 = _interopRequireDefault(_Point);
 
@@ -19132,7 +18882,7 @@ var Dice = function (_React$Component) {
 exports.default = Dice;
 
 /***/ }),
-/* 41 */
+/* 39 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -19206,7 +18956,7 @@ var pointsHandler = { showMarkedPoints: showMarkedPoints, hideAll: hideAll };
 exports.default = pointsHandler;
 
 /***/ }),
-/* 42 */
+/* 40 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -19229,7 +18979,7 @@ var Point = function Point() {
 exports.default = Point;
 
 /***/ }),
-/* 43 */
+/* 41 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -19243,7 +18993,7 @@ var POINTS_FOR_WIN = 100;
 exports.default = POINTS_FOR_WIN;
 
 /***/ }),
-/* 44 */
+/* 42 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -19268,7 +19018,7 @@ var services = {
 exports.default = services;
 
 /***/ }),
-/* 45 */
+/* 43 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -19295,13 +19045,13 @@ var initialState = {
 exports.default = initialState;
 
 /***/ }),
-/* 46 */
+/* 44 */
 /***/ (function(module, exports, __webpack_require__) {
 
 // style-loader: Adds some css to the DOM by adding a <style> tag
 
 // load the styles
-var content = __webpack_require__(47);
+var content = __webpack_require__(45);
 if(typeof content === 'string') content = [[module.i, content, '']];
 // Prepare cssTransformation
 var transform;
@@ -19309,7 +19059,7 @@ var transform;
 var options = {"hmr":true}
 options.transform = transform
 // add the styles to the DOM
-var update = __webpack_require__(49)(content, options);
+var update = __webpack_require__(47)(content, options);
 if(content.locals) module.exports = content.locals;
 // Hot Module Replacement
 if(false) {
@@ -19326,21 +19076,21 @@ if(false) {
 }
 
 /***/ }),
-/* 47 */
+/* 45 */
 /***/ (function(module, exports, __webpack_require__) {
 
-exports = module.exports = __webpack_require__(48)(undefined);
+exports = module.exports = __webpack_require__(46)(undefined);
 // imports
 
 
 // module
-exports.push([module.i, "body {\n  margin: 0px !important;\n  overflow: hidden;\n  font-family: 'Spectral SC', serif;\n}\n\n#pig_dice_game {\n  width: 100%;\n  height: 100%;\n  border: 1px solid black;\n}\n\n.interface-container {\n  width: 100%;\n  height: 100%;\n  display: flex;\n  justify-content: center;\n  align-items: center;\n  position: relative;\n}\n\n.players-container {\n  width: 100%;\n  height: 90%;\n  display: flex;\n  justify-content: space-between;\n  align-items: flex-start;\n  font-size: 2vw;\n}\n.players-container > div {\n  display:flex;\n  justify-content: center;\n  flex-wrap: wrap;\n}\n\n.players-container > div > p {\n  width: 60%;\n  display: flex;\n  justify-content: center;\n}\n\n.bottom-control-container {\n  width: 24%;\n  height:20%;\n  display: flex;\n  justify-content: center;\n  align-items: flex-end;\n  flex-wrap: wrap;\n  position: absolute;\n  bottom: 10%;\n  font-size: 2.2vw;\n}\n\n.upper-control-container {\n  width: 24%;\n  height:20%;\n  display: flex;\n  justify-content: center;\n  align-items: flex-start;\n  position: absolute;\n  top: 0px;\n}\n\nbutton {\n  cursor: pointer;\n}\n\n.btn {\n  width: 100%;\n  color: white;\n  border-radius: 4px;\n  text-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);\n  background: rgb(66, 184, 221);\n  font-size: 2.2vw;\n}\n\n.btn-reference {\n  width: 3vw;\n  height: 3vw;\n  position: absolute;\n  right: 0px;\n  top: 0px;\n}\n\n.player-area {\n  width: 30%;\n}\n\n.active-player {\n  color: green;\n  font-weight: bolder;\n}\n\n.dice {\n  width: 20vw;\n  height: 20vw;\n  display: flex;\n  justify-content: space-between;\n  flex-wrap: wrap;\n  position: absolute;\n}\n\n.point {\n  width: 30%;\n  height: 30%;\n  background-color: black;\n  border-radius: 50%;\n}\n\n.hidden {\n  visibility: hidden;\n}\n\n.disabled-block {\n  pointer-events: none;\n  opacity: 0.4;\n}\n", ""]);
+exports.push([module.i, "body {\n  margin: 0px !important;\n  overflow: hidden;\n  font-family: 'Spectral SC', serif;\n}\n\n#pig_dice_game {\n  width: 100%;\n  height: 100%;\n  border: 1px solid black;\n}\n\n.interface-container {\n  width: 100%;\n  height: 100%;\n  display: flex;\n  justify-content: center;\n  align-items: center;\n  position: relative;\n}\n\n.players-container {\n  width: 100%;\n  height: 90%;\n  display: flex;\n  justify-content: space-between;\n  align-items: flex-start;\n  font-size: 2vw;\n}\n.players-container > div {\n  display:flex;\n  justify-content: center;\n  flex-wrap: wrap;\n}\n\n.players-container > div > p {\n  width: 60%;\n  display: flex;\n  justify-content: center;\n}\n\n.bottom-control-container {\n  width: 24%;\n  height:20%;\n  display: flex;\n  justify-content: center;\n  align-items: flex-end;\n  flex-wrap: wrap;\n  position: absolute;\n  bottom: 10%;\n  font-size: 2.2vw;\n}\n\n.upper-control-container {\n  width: 24%;\n  height:20%;\n  display: flex;\n  justify-content: center;\n  align-items: flex-start;\n  position: absolute;\n  top: 0px;\n}\n\nbutton {\n  cursor: pointer;\n}\n\n.btn {\n  width: 100%;\n  color: white;\n  border-radius: 4px;\n  text-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);\n  background: rgb(66, 184, 221);\n  font-size: 2.2vw;\n}\n\n.btn-reference {\n  width: 3vw;\n  height: 3vw;\n  position: absolute;\n  right: 0px;\n  top: 0px;\n}\n\n.player-area {\n  width: 34%;\n}\n\n.active-player {\n  color: green;\n  font-weight: bolder;\n}\n\n.dice {\n  width: 20vw;\n  height: 20vw;\n  display: flex;\n  justify-content: space-between;\n  flex-wrap: wrap;\n  position: absolute;\n}\n\n.point {\n  width: 30%;\n  height: 30%;\n  background-color: black;\n  border-radius: 50%;\n}\n\n.hidden {\n  visibility: hidden;\n}\n\n.disabled-block {\n  pointer-events: none;\n  opacity: 0.4;\n}\n", ""]);
 
 // exports
 
 
 /***/ }),
-/* 48 */
+/* 46 */
 /***/ (function(module, exports) {
 
 /*
@@ -19422,7 +19172,7 @@ function toComment(sourceMap) {
 
 
 /***/ }),
-/* 49 */
+/* 47 */
 /***/ (function(module, exports, __webpack_require__) {
 
 /*
@@ -19478,7 +19228,7 @@ var singleton = null;
 var	singletonCounter = 0;
 var	stylesInsertedAtTop = [];
 
-var	fixUrls = __webpack_require__(50);
+var	fixUrls = __webpack_require__(48);
 
 module.exports = function(list, options) {
 	if (typeof DEBUG !== "undefined" && DEBUG) {
@@ -19794,7 +19544,7 @@ function updateLink (link, options, obj) {
 
 
 /***/ }),
-/* 50 */
+/* 48 */
 /***/ (function(module, exports) {
 
 
